98002be2a09650496de44c6a61fba539a22d1df7
JW <choi97201@naver.com>
2020-01-17 11:24:17 +0900

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='greedyNorm',
+    version='1.0',
+    url='https://github.com/choi97201/greedyNorm',
+    author='cjw',
+    author_email='choi97201@naver.com',
+    packages=find_packages(),
+    zip_safe=False,
+    python_requires='>=3'
+)
